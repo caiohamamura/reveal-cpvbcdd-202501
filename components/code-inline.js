a034b23c355613bf5036b62dcd0838c20d71cd73
Caio Hamamura <caiohamamura@gmail.com>
2025-06-03 21:09:57 -0300

    Adicionado isolamento e durabilidade

--- components/code-inline.js
+++ components/code-inline.js
@@ -10,3 +10,15 @@ window.app.component("code-inline", {
   <pre class="code-inline"><code :class="classNames" :style="{fontSize: (size || 20) + 'pt'}" data-trim><slot></slot></code></pre>
         `,
 });
+
+// Columns wrapper (supports 2-3 columns)
+app.component('multi-col', {
+  template: `<div class="multi-col" :style="{ '--col-count': cols }"><slot></slot></div>`,
+  props: { cols: { type: Number, default: 2 } }
+});
+
+// Highlight box component
+app.component('highlight-box', {
+  template: `<div class="fragment highlight-box" :data-fragment-index="index"><slot></slot></div>`,
+  props: ['index']
+});
